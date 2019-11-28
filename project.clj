22748d1b282412e711fb0ad213792333b1ba6343
Oleg Tsvetkov <oleg.tsvetkov@tallink.ee>
2019-11-28 12:18:39 +0200

    Update project url and description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject chrome-driver-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "chrome driver downloader"
+  :url "https://github.com/olecve/chrome-driver-clj"
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns chrome-driver-clj.core})
