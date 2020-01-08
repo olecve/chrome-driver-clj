20a93d9ac9dd9c86597f8cd52a96624886c72a61
Oleg Tsvetkov <olecve@gmail.com>
2020-01-08 14:48:25 +0200

    Add clean function and test

--- src/chrome_driver_clj/core.clj
+++ src/chrome_driver_clj/core.clj
@@ -4,6 +4,7 @@
   (:import (java.io File)
            (java.util.zip ZipInputStream)))
 
+(def ^:private location-key "webdriver.chrome.driver")
 (def ^:private temp-dir (System/getProperty "java.io.tmpdir"))
 (def ^:private root-uri "http://chromedriver.storage.googleapis.com/")
 (def ^:private latest-release-version-uri (str root-uri "LATEST_RELEASE"))
@@ -31,23 +32,36 @@
     (includes? os-name "mac") {:file-name (str "chromedriver-" driver-version) :os "mac64"}
     :else (throw (RuntimeException. "Can't identify operating system"))))
 
-(defn- locate-driver [driver-version]
+(defn- download-driver [os driver-version zip bin]
+  (println (str "[ Downloading chrome driver for " os " with version " driver-version " ]"))
+  (download (create-driver-uri os driver-version) zip)
+  (println "[ Driver is downloaded, unzipping... ]")
+  (unzip zip bin)
+  (.setExecutable (io/file bin) true)
+  (println "[ Driver is ready ]"))
+
+(defn- locate-or-download-driver [version]
   (let [os-name (-> "os.name" System/getProperty lower-case)
-        {:keys [file-name os]} (os-name->file-and-os-names os-name driver-version)
-        path-to-driver (str temp-dir File/separator file-name)
-        path-to-zip (str temp-dir File/separator "chromedriver_" os ".zip")]
-    (when-not (file-exists? path-to-driver)
-      (do
-        (println (str "[ Downloading chrome driver for " os " with version " driver-version " ]"))
-        (download (create-driver-uri os driver-version) path-to-zip)
-        (println "[ Driver is downloaded, unzipping... ]")
-        (unzip path-to-zip path-to-driver)
-        (.setExecutable (io/file path-to-driver) true)
-        (println "[ Driver is ready ]")))
-    path-to-driver))
+        {:keys [file-name os]} (os-name->file-and-os-names os-name version)
+        bin (str temp-dir File/separator file-name)
+        zip (str temp-dir File/separator "chromedriver_" os ".zip")
+        file-exists (file-exists? bin)]
+    (when-not file-exists (download-driver os version zip bin))
+    {:bin         bin
+     :zip         zip
+     :file-exists file-exists}))
 
 (defn init []
-  (let [driver-version (get-latest-release-version)
-        path-to-driver (locate-driver driver-version)]
-    (System/setProperty "webdriver.chrome.driver" path-to-driver)
-    path-to-driver))
+  (let [version (get-latest-release-version)
+        paths (locate-or-download-driver version)]
+    (System/setProperty location-key (:bin paths))
+    (merge paths
+           {:version version})))
+
+(defn clean []
+  (let [files (->> temp-dir
+                   io/file
+                   file-seq
+                   (filter #(re-find #"chromedriver" (.getName %))))]
+    (doseq [file files]
+      (io/delete-file file))))
--- test/chrome_driver_clj/core_test.clj
+++ test/chrome_driver_clj/core_test.clj
@@ -1,7 +1,36 @@
 (ns chrome-driver-clj.core-test
-  (:require [chrome-driver-clj.core :refer :all]
+  (:require [clojure.java.io :as io]
+            [chrome-driver-clj.core :as chrome-driver]
             [clojure.test :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def location-key "webdriver.chrome.driver")
+(defn get-path [] (System/getProperty location-key))
+
+(defn with-clean-property-fixture [f]
+  (chrome-driver/clean)
+  (System/clearProperty location-key)
+  (f)
+  (System/clearProperty location-key))
+
+(use-fixtures :once with-clean-property-fixture)
+
+(deftest init-test
+  (is (nil? (get-path)))
+
+  (let [result (chrome-driver/init)
+        path (get-path)]
+    (is (not (nil? path)))
+    (is (= (:bin result) path))
+    (is (false? (:file-exists result)))
+    (is (some? (:version result)))
+    (is (.exists (io/file path)))
+    (is (.isFile (io/file path))))
+
+  (let [result (chrome-driver/init)
+        path (get-path)]
+    (is (not (nil? path)))
+    (is (= (:bin result) path))
+    (is (true? (:file-exists result)))
+    (is (some? (:version result)))
+    (is (.exists (io/file path)))
+    (is (.isFile (io/file path)))))
