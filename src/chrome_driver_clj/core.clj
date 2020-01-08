5a018f99ceda79422222589cc5573531cb377a07
Oleg Tsvetkov <olecve@gmail.com>
2020-01-08 12:48:26 +0200

    Get latest release version and download it

--- src/chrome_driver_clj/core.clj
+++ src/chrome_driver_clj/core.clj
@@ -4,8 +4,11 @@
   (:import (java.io File)
            (java.util.zip ZipInputStream)))
 
-(def ^:private driver-version "78.0.3904.70")
 (def ^:private temp-dir (System/getProperty "java.io.tmpdir"))
+(def ^:private root-uri "http://chromedriver.storage.googleapis.com/")
+(def ^:private latest-release-version-uri (str root-uri "LATEST_RELEASE"))
+
+(defn- get-latest-release-version [] (slurp latest-release-version-uri))
 
 (defn- copy [uri file]
   (with-open [in (io/input-stream uri)
@@ -25,16 +28,16 @@
 (defn- file-exists? [absolute-path]
   (.exists (io/as-file absolute-path)))
 
-(defn- os-name->file-and-os-names [os-name]
+(defn- os-name->file-and-os-names [os-name driver-version]
   (cond
     (includes? os-name "win") {:file-name (str "chromedriver-" driver-version ".exe") :os "win32"}
     (includes? os-name "nux") {:file-name (str "chromedriver-" driver-version) :os "linux64"}
     (includes? os-name "mac") {:file-name (str "chromedriver-" driver-version) :os "mac64"}
     :else (throw (Exception. "Can't recognize operating system"))))
 
-(defn- locate-driver []
+(defn- locate-driver [driver-version]
   (let [os-name (-> "os.name" System/getProperty lower-case)
-        {:keys [file-name os]} (os-name->file-and-os-names os-name)
+        {:keys [file-name os]} (os-name->file-and-os-names os-name driver-version)
         path-to-driver (str temp-dir File/separator file-name)
         path-to-zip (str temp-dir File/separator "chromedriver_" os ".zip")]
     (when-not (file-exists? path-to-driver)
@@ -48,6 +51,7 @@
     path-to-driver))
 
 (defn init []
-  (let [path-to-driver (locate-driver)]
+  (let [driver-version (get-latest-release-version)
+        path-to-driver (locate-driver driver-version)]
     (System/setProperty "webdriver.chrome.driver" path-to-driver)
     path-to-driver))
